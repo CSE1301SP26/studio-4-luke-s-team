071cdd1030ecb1c7069481a1ee3118b5d5bb3e8a
lukemitchell4 <m.luke@wustl.edu>
2026-02-18 15:36:37 -0600

    complete

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,16 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.rectangle(0.5,0.5,0.4,0.25);
+		StdDraw.setPenColor(193, 235, 255);
+		StdDraw.filledRectangle(0.2,0.35,0.1,0.1);
+		StdDraw.filledRectangle(0.8,0.35,0.1,0.1);
+		StdDraw.filledRectangle(0.2,0.65,0.1,0.1);
+		StdDraw.filledRectangle(0.8,0.65,0.1,0.1);
+		StdDraw.setPenColor(255,0,0);
+		StdDraw.filledEllipse(0.5,0.5,0.2,0.2);
+		StdDraw.setPenColor(0,204,0);
+		StdDraw.filledRectangle(0.5, 0.5, 0.1, 0.1);
 	}
 }
